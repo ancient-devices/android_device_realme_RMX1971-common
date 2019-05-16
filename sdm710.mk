876f09be5ccc9ee17e4c317747f4b87482e34dc7
Jun Wang <junwan@codeaurora.org>
2019-05-16 11:27:45 +0800

    sdm710-common: Add atrace-hal build rule
    CRs-Fixed: 2453668
    Change-Id: If995d1bf5cae68e09c156ac297fd7a5f5ca0d209
    Signed-off-by: AkaruiAikara <vall@aikara.me>

--- sdm710.mk
+++ sdm710.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# ATrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
