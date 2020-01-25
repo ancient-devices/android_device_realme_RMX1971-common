a4dad1e2d73f820eda4bdb3e59a357f83f11a414
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    sdm710-common: Use Pixel offline charging animation
    Signed-off-by: AkaruiAikara <vall@aikara.me>

--- sdm710.mk
+++ sdm710.mk
@@ -314,6 +314,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
