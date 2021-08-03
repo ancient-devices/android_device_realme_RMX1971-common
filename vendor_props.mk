0adc91f845f85ffc3bc361535bdc8be0b01919a7
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-08-03 21:18:02 +0000

    sdm710-common: libinit: handle dalvik props based on sysram
    * Adapted to Realme sdm710.
    * We have 3 variants(4gb, 6gb & 8gb RAM)
    * Values are taken from frameworks/native
    
    Co-authored-by: kaderbava <ksbava7325@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -62,16 +62,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.gyro.disable=0
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat64.enabled=true \
-    dalvik.vm.heapgrowthlimit=384m \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=16m
-
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/OPPO_OLED_AD_calib.cfg \
